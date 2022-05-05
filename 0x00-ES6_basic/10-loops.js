418830426bdbc64556f21f706fc15eaa7cef2d7a
Scatmandu <mcclur05@nsuok.edu>
2022-05-05 14:50:58 -0500

    was using for of wrong, changed line 4

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
-  for (const idx of array) {
-    const toAppend = appendString + array[idx];
+  for (let idx of array) {
+    const toAppend = appendString + idx
     newArray.push(toAppend);
   }
 
