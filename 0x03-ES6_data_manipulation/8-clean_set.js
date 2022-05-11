3ac0b3f6ed566d30aca9f71c062d7c4fb162858c
Scatmandu <mcclur05@nsuok.edu>
2022-05-10 23:49:50 -0500

    moved stuff around in 8 and added 9

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,14 @@
 export default function cleanSet(set, startString) {
+  if (startString === '' || typeof startString !== 'string') {
+    return '';
+  }
+
   const newArray = [];
+
   for (const element of set) {
     if (element.startsWith(startString) && typeof element === 'string') {
       newArray.push(element.slice(startString.length));
     }
-  }
-  if (startString === '' || typeof startString !== 'string') {
-    return '';
   }
   return newArray.join('-');
 }
