65c517a321972e7b91fba98023945800d12cc8a0
Scatmandu <mcclur05@nsuok.edu>
2022-05-10 23:46:00 -0500

    added checks for types

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,11 @@
 export default function cleanSet(set, startString) {
   const newArray = [];
   for (const element of set) {
-    if (element.startsWith(startString)) {
+    if (element.startsWith(startString) && typeof element === 'string') {
       newArray.push(element.slice(startString.length));
     }
   }
-  if (startString === '') {
+  if (startString === '' || typeof startString !== 'string') {
     return '';
   }
   return newArray.join('-');
